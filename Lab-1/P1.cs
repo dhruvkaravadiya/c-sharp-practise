46858a2edd7070b52b7ea4ddfb9933c77e8b9c80
Dhruv Karavadiya <94845676+dhruvkaravadiya@users.noreply.github.com>
2023-07-04 17:15:27 +0530

    Create P1.cs

---
+++ Lab-1/P1.cs
@@ -0,0 +1,16 @@
+// Print Name, Address, Contact Number, and City
+class P1
+{
+    static void Main()
+    {
+        string name = "Your Name";
+        string address = "Your Address";
+        string contactNumber = "Your Contact Number";
+        string city = "Your City";
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Address: " + address);
+        Console.WriteLine("Contact Number: " + contactNumber);
+        Console.WriteLine("City: " + city);
+    }
+}
